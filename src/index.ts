bf4034ee6ad3fe67d7e036709ac8140013c73dca
Dmytro Mykhailiuk <dmytrom@ravin-ai.com>
2023-03-10 18:15:32 +0200

    asda

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,4 @@ export * from './condition-functions';
 export * from './condition-operators';
 export * from './flow-runner';
 export * from './interfaces';
-
 export * from './validate-conditions-and-run-flow';
